8f5e979529e10e5cba490bf7c7ae950a9a17f122
moransh4 <moransh4@gmail.com>
2023-11-05 23:15:56 +0200

    changes

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-let section = document.querySelectorAll('section');
-let menu = document.querySelectorAll('#navbar .links a');
-document.querySelector('#navbar .links a').classList.add('active');
-menu.forEach((link) => {
-  link.addEventListener('click', (event) => {
-      event.preventDefault();
-      // document.querySelector('#navbar .links a.active').classList.remove('active');
-      // link.classList.add('active');
-      link.classList.remove('active');
-      var sectionId = link.name;
-      document.getElementById(sectionId).scrollIntoView();
-  });
-});
+// let section = document.querySelectorAll('section');
+// let menu = document.querySelectorAll('#navbar .links a');
+// document.querySelector('#navbar .links a').classList.add('active');
+// menu.forEach((link) => {
+//   link.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       // document.querySelector('#navbar .links a.active').classList.remove('active');
+//       // link.classList.add('active');
+//       link.classList.remove('active');
+//       var sectionId = link.name;
+//       document.getElementById(sectionId).scrollIntoView();
+//   });
+// });
 
-  section.forEach(i => {
-    let top = window.scrollY;
-    let offset = i.offsetTop;
-    let height = i.offsetHeight;
-    let id = i.getAttribute('id');
-    if (top >= offset && top < offset + height) {
-      menu.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector('#navbar .links a[href*=' + id + ']').classList.add('active');
-      });
-    }
+//   section.forEach(i => {
+//     let top = window.scrollY;
+//     let offset = i.offsetTop;
+//     let height = i.offsetHeight;
+//     let id = i.getAttribute('id');
+//     if (top >= offset && top < offset + height) {
+//       menu.forEach(link => {
+//         link.classList.remove('active');
+//         document.querySelector('#navbar .links a[href*=' + id + ']').classList.add('active');
+//       });
+//     }
+//   });
+
+  const menuButton = document.getElementsByClassName('menu-button');
+  const menu = document.getElementsByClassName('header-navigation');
+
+  menuButton.addEventListener('click', () => {
+      menuButton.classList.toggle('active');
+      menu.classList.toggle('active');
   });
+
