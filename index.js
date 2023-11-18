75ed7ff49db94a49853f8752077757b07e4f3cdb
moransh4 <moransh4@gmail.com>
2023-11-18 08:36:12 +0200

    updates text and images

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ s
       menu.classList.toggle('active');
   });
 
+// //  Initialize Swiper
+// let swiper = new Swiper(".mySwiper", {
+//   lazy: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   keyboard: {
+//     enabled: true,
+//   },
+//   mousewheel: true,
+//   grabCursor: true,
+
+//  });
