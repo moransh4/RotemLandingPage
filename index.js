9d226d849aee17e38dc0b052cf0c3e38bc13bf67
moransh4 <moransh4@gmail.com>
2023-12-06 14:22:37 +0200

    fix - add changes

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-s
-  const menuButton = document.getElementsByClassName('menu-button');
-  const menu = document.getElementsByClassName('header-navigation');
 
-  menuButton.addEventListener('click', () => {
-      menuButton.classList.toggle('active');
-      menu.classList.toggle('active');
-  });
+  // const menuButton = document.getElementsByClassName('menu-button');
+  // const menu = document.getElementsByClassName('header-navigation');
 
-// //  Initialize Swiper
-// let swiper = new Swiper(".mySwiper", {
-//   lazy: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   keyboard: {
-//     enabled: true,
-//   },
-//   mousewheel: true,
-//   grabCursor: true,
+  // menuButton.addEventListener('click', () => {
+  //     menuButton.classList.toggle('active');
+  //     menu.classList.toggle('active');
+  // });
 
-//  });
+  let swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    lazy: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+   });
